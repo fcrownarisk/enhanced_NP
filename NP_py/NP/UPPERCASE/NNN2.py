8148b532368dc6906bd718140f657ef22da83923
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-25 13:06:03 +0800

    Add files via upload

---
+++ NP_py/NP/UPPERCASE/NNN2.py
@@ -0,0 +1,9 @@
+import junction.base
+import junction.collector
+import junction.emitter
+def NNN():
+ emitter =   '|_|_|_|'
+ collector = '|-/ \-|'
+ base =      '|     |'
+ collector = '|_\ /_|'
+ emitter =   '|_|_|_|'
---
+++ NP_py/NP/lowercase/NPN.py
@@ -0,0 +1,9 @@
+import junction.base
+import junction.collector
+import junction.emitter
+def NPN():
+ emitter =   '|_|_|_|'
+ collector = '|_\ /_|'
+ base =      '|     |'
+ collector = '|-\ /-|'
+ emitter =   '|_|_|_|'
