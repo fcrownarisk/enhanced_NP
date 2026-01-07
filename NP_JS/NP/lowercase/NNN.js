289f796c0fab3702914e65f64f45c38f38b5c908
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2026-01-07 22:46:43 +0800

    Update NNN.js

--- NP_JS/NP/lowercase/NNN.js
+++ NP_JS/NP/lowercase/NNN.js
@@ -1,4 +1,4 @@
-//NNNN
+//NNNP
 import 'junction/base'
 import 'junction/collector'
 import 'junction/emitter'
@@ -6,6 +6,6 @@ export function NNN(){
  emitter =   '|_|_|_|'
  collector = '|_\ \_|'
  base =      '|     |'
- collector = '|-\ \-|'
+ collector = '|-\ /-|'
  emitter =   '|_|_|_|'
 }
